32f57084a8ade63a1769c8cfc19ca9ec46bb270e
molucorner <moa.lundkvist@regionvasterbotten.se>
2026-01-22 16:45:27 +0100

    linting

--- actions/make_case_id.py
+++ actions/make_case_id.py
@@ -4,6 +4,7 @@ from st2common.runners.base_action import Action
 from wonderwords import RandomWord
 
 
+
 class MakeCaseID(Action):
     def run(self, samples):
         word_generator = RandomWord()
