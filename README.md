fe6758b2e22842f5f2a8e4e1ca2d263e7b861728
molucorner <moa.lundkvist@regionvasterbotten.se>
2026-03-06 10:09:44 +0100

    feat: add `make case id` action (#47)

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ ref                                               | description
 gmc_norr_analysis.generate_tumor_evolution_report | Generate a tumor evolution report from an Excel file
 gmc_norr_analysis.tumor_evolution                 | Workflow for generating a tumor evolution report
 gmc_norr_analysis.write_file                      | Write a text string to a file
+gmc_norr_analysis.make_case_id                    | Make a case id of random words and a hash of sample ids
 gmc_norr_analysis.make_raredisease_samplesheet    | Make a samplesheet for the nf-core/raredisease pipeline
 
 ## Rules
---
+++ actions/make_case_id.py
@@ -0,0 +1,19 @@
+import hashlib
+
+from st2common.runners.base_action import Action
+from wonderwords import RandomWord
+
+
+class MakeCaseID(Action):
+    def run(self, samples):
+        word_generator = RandomWord()
+
+        case_id_adj = word_generator.word(include_categories=["adjective"]).strip()
+        case_id_noun = word_generator.word(include_categories=["noun"]).strip()
+        case_id = f"{case_id_adj}_{case_id_noun}".replace(
+            "'", "").replace("-", "_").replace(" ", "_")
+
+        sample_bytes = bytes(" ".join(sorted(samples)), "utf-8")
+        hash_id = hashlib.md5(sample_bytes).hexdigest()[:8]
+
+        return (True,  case_id + "_" + hash_id)
