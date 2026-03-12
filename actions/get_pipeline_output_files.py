939e4027539217c29b1af863c1af61a198e12a6f
molucorner <moa.lundkvist@regionvasterbotten.se>
2026-03-12 11:11:13 +0100

    use python datatypes first and tehn dump to string (and switch around quotation marks)

--- actions/get_pipeline_output_files.py
+++ actions/get_pipeline_output_files.py
@@ -1,35 +1,35 @@
 from st2common.runners.base_action import Action
+import json
 
 
 def get_rd_output_files(sample_id, case_id):
-    output_files = "[{'path': 'raredisease_results/multiqc/multiqc_report.html', " \
-                "'level': 'sample', 'type': 'html', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/peddy/" + case_id + ".peddy.ped', " \
-                "'level': 'sample', 'type': 'text', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/peddy/" + case_id + ".sex_check.csv', " \
-                "'level': 'sample', 'type': 'text', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/peddy/" + case_id + ".ped_check.csv', " \
-                "'level': 'sample', 'type': 'text', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/alignment/" + sample_id + "_sorted_md.bam', " \
-                "'level': 'sample', 'type': 'bam', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/qc_bam/" + sample_id + "_mosdepth.per-base.d4', " \
-                "'level': 'sample', 'type': 'd4', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/annotate_snv/genome/" + sample_id + \
-                "_rhocallviz_autozyg_chromograph/" + sample_id + "_rhocallviz_chr*.png'," \
-                "'level': 'sample', 'type': 'png', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/qc_bam/" + sample_id + "_chromographcov/" \
-                "" + sample_id + "_tidditcov_chr*.png', " \
-                "'level': 'sample', 'type': 'png', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/smncopynumbercaller/out/" \
-                "" + case_id + "_smncopynumbercaller.tsv', " \
-                "'level': 'sample', 'type': 'text', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/rank_and_filter/" + case_id + "" \
-                "_snv_ranked_clinical.vcf.gz', " \
-                "'level': 'sample', 'type': 'vcf_snv', 'parent_id': '" + sample_id + "'}, " \
-                "{'path': 'raredisease_results/rank_and_filter/" \
-                "" + case_id + "_snv_ranked_research.vcf.gz', " \
-                "'level': 'sample', 'type': 'vcf_snv', 'parent_id': '" + sample_id + "'},]"
-    return output_files
+    output_files = [
+        {'path': 'raredisease_results/multiqc/multiqc_report.html',
+         'level': 'sample', 'type': 'html', 'parent_id': sample_id},
+        {'path': f'raredisease_results/peddy/{case_id}.peddy.ped',
+         'level': 'sample', 'type': 'text', 'parent_id': sample_id},
+        {'path': f'raredisease_results/peddy/{case_id}.sex_check.csv',
+         'level': 'sample', 'type': 'text', 'parent_id': sample_id},
+        {'path': f'raredisease_results/peddy/{case_id}.ped_check.csv',
+         'level': 'sample', 'type': 'text', 'parent_id': sample_id},
+        {'path': f'raredisease_results/alignment/{sample_id}_sorted_md.bam',
+         'level': 'sample', 'type': 'bam', 'parent_id': sample_id},
+        {'path': f'raredisease_results/qc_bam/{sample_id}_mosdepth.per-base.d4',
+         'level': 'sample', 'type': 'd4', 'parent_id': sample_id},
+        {'path': 'raredisease_results/annotate_snv/genome/' +
+         f'{sample_id}_rhocallviz_autozyg_chromograph/{sample_id}_rhocallviz_chr*.png',
+         'level': 'sample', 'type': 'png', 'parent_id': sample_id},
+        {'path': f'raredisease_results/qc_bam/{sample_id}_chromographcov/'
+         + f'{sample_id}_tidditcov_chr*.png',
+         'level': 'sample', 'type': 'png', 'parent_id': sample_id},
+        {'path': f'raredisease_results/smncopynumbercaller/out/{case_id}_smncopynumbercaller.tsv',
+         'level': 'sample', 'type': 'text', 'parent_id': sample_id},
+        {'path': f'raredisease_results/rank_and_filter/{case_id}_snv_ranked_clinical.vcf.gz',
+         'level': 'sample', 'type': 'vcf_snv', 'parent_id': sample_id},
+        {'path': f'raredisease_results/rank_and_filter/{case_id}_snv_ranked_research.vcf.gz',
+         'level': 'sample', 'type': 'vcf_snv', 'parent_id': sample_id}
+         ]
+    return json.dumps(output_files)
 
 
 class GetPipelineOutputFiles(Action):
@@ -38,4 +38,4 @@ class GetPipelineOutputFiles(Action):
             output_files = get_rd_output_files(sample_id, case_id)
         else:
             return (False, "unsupported pipeline")
-        return (True,  output_files)
+        return (True,  output_files.replace('"', '@').replace("'", '"').replace("@", "'"))
