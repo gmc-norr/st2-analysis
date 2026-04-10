971961a1ca6088c29369bfd42a5850062836d65b
molucorner <moa.lundkvist@regionvasterbotten.se>
2026-04-10 14:12:02 +0200

    Check number of fastq files when making analysis input for Ceve

--- actions/get_pipeline_input_files.py
+++ actions/get_pipeline_input_files.py
@@ -11,6 +11,10 @@ def get_rd_input_files(sample_id, analysis_id, fastq_files):
 
 class GetPipelineInputFilesAction(Action):
     def run(self, pipeline, fastq_files, analysis_ids, sample_id):
+        if len(fastq_files[sample_id]["R1"]) != len(fastq_files[sample_id]["R2"]):
+            return (False, "The number of R1 and R2 fastq files don't match")
+        if len(fastq_files[sample_id]["R1"]) < 1:
+            return (False, "No fastq files received")
         if pipeline == "nf-core/raredisease":
             input_files = get_rd_input_files(sample_id, analysis_ids[sample_id],
                                              fastq_files[sample_id])
